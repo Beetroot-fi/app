b22f2a77a2778a187e2551ac9a27324cb9f37458
welaskez <nazar.nikitenko.03@mail.ru>
2024-10-30 02:05:10 +0500

    feat: add fetching tasks & completed tasks from api

--- src/pages/Tasks/ui/Tasks.tsx
+++ src/pages/Tasks/ui/Tasks.tsx
@@ -1,79 +1,69 @@
 import clsx from "clsx";
 import { ArrowRightIcon } from "../../../components/Icons/ArrowRightIcon";
 import s from "./Tasks.module.scss";
+import { useEffect, useState } from "react";
+import { apiService, TaskRead } from "../../../api";
 import { ThisInviteLinkIcon } from "../../../components/Icons/ThisInviteLinkIcon";
 
 export const Tasks = () => {
+  const [tasks, setTasks] = useState<TaskRead[]>([]);
+  const [completedTasks, setCompletedTasks] = useState<TaskRead[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const tasks = (await apiService.getTasks()).data;
+        setTasks(tasks);
+
+        const completedTasks = (await apiService.getCompletedTasks()).data;
+        setCompletedTasks(completedTasks);
+      } catch (error) {
+        console.error("Error fetching:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className={s.wrapper}>
       <div className={s.block}>
-        <div className={s.block_title}>new task:</div>
+        <div className={s.block_title}>tasks:</div>
         <div className={s.block_items}>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ArrowRightIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ArrowRightIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ArrowRightIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ArrowRightIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ArrowRightIcon />
-            </div>
-          </div>
+          {tasks.length > 0 ? (
+            tasks.map((task, index) => (
+              <div className={s.block_item} key={index}>
+                <div className={s.block_item_l}>{task.name}</div>
+                <div className={s.block_item_r}>
+                  <ArrowRightIcon />
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className={s.no_referrals}>No tasks yet</div>
+          )}
         </div>
       </div>
       <div className={s.block}>
         <div className={s.block_title}>completed tasks:</div>
         <div className={clsx(s.block_items, s.pink)}>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ThisInviteLinkIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ThisInviteLinkIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ThisInviteLinkIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ThisInviteLinkIcon />
-            </div>
-          </div>
-          <div className={s.block_item}>
-            <div className={s.block_item_l}>follow x @beetroot</div>
-            <div className={s.block_item_r}>
-              <ThisInviteLinkIcon />
-            </div>
-          </div>
+          {completedTasks.length > 0 ? (
+            completedTasks.map((task, index) => (
+              <div className={s.block_item} key={index}>
+                <div className={s.block_item_l}>{task.name}</div>
+                <div className={s.block_item_r}>
+                  <ThisInviteLinkIcon />
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className={s.no_referrals}>No completed tasks yet</div>
+          )}
         </div>
       </div>
     </div>
