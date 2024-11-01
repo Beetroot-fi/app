8091ee8d7eb457e2728ee1b272b7ec63712028ed
1manaliyev <baxtik.kkp@mail.ru>
2024-11-01 15:19:54 +0500

    corrected the display of the task

--- src/pages/Tasks/ui/Tasks.tsx
+++ src/pages/Tasks/ui/Tasks.tsx
@@ -39,11 +39,9 @@ export const Tasks = () => {
           {tasks.length > 0 ? (
             tasks.map((task, index) => (
               <Link to={task.link} className={s.block_item} key={index}>
-                <div className={s.block_item}>
-                  <div className={s.block_item_l}>{task.name}</div>
-                  <div className={s.block_item_r}>
-                    <ArrowRightIcon />
-                  </div>
+                <div className={s.block_item_l}>{task.name}</div>
+                <div className={s.block_item_r}>
+                  <ArrowRightIcon />
                 </div>
               </Link>
             ))
