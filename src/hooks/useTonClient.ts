7a525bf3eca42adc07294253da96ed209908cfb0
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-01 19:55:06 +0500

    feat: switch to toncenter

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -1,14 +1,13 @@
 
 import { TonClient } from "@ton/ton";
 import useAsyncInitialize from "./useAsyncInitialize";
-import { getHttpEndpoint } from "@orbs-network/ton-access";
 
 export default function useTonClient() {
     return useAsyncInitialize(
         async () =>
             new TonClient({
-                endpoint: await getHttpEndpoint({ network: "testnet" }),
-                // apiKey: import.meta.env.VITE_TONCENTER_API_KEY,
+                endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC",
+                apiKey: import.meta.env.VITE_TONCENTER_API_KEY,
             }),
     )
 }
