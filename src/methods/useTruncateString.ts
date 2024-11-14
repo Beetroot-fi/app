16bcdd6e8b4e2de5cc35a7060ee78786e9cb0ab2
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-14 23:43:55 +0500

    refactor: remove comments

--- src/methods/useTruncateString.ts
+++ src/methods/useTruncateString.ts
@@ -1,18 +1,14 @@
-// useTruncateString.ts
 function useTruncateString(
   input: string | null,
   start: number,
   end: number
 ): string {
-  // Проверяем, что входная строка не null и не undefined
   if (input == null) {
-    return ""; // Возвращаем пустую строку или другое уместное значение
+    return "";
   }
 
-  // Теперь можно безопасно обращаться к свойству length
   const length = input.length;
 
-  // Трункация строки
   if (length > start + end) {
     return `${input.substring(0, start)}...${input.substring(
       length - end,
