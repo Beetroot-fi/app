d31a56802a77a8a6e28df64837ad0372d966d00c
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-15 00:53:02 +0500

    refactor: delete unused code from jetton utils

--- src/methods/jettonUtils.ts
+++ src/methods/jettonUtils.ts
@@ -1,27 +1,4 @@
-import { beginCell, Address, Cell, TonClient } from "@ton/ton";
-import * as op from "../op";
-
-
-export function buildJettonTransferBody(
-    queryId: bigint,
-    jettonAmount: bigint,
-    to: Address,
-    responseAddress: Address,
-    customPayload: Cell | null,
-    forwardTonAmount: bigint,
-    forwardPayload: Cell | null,
-): Cell {
-    return beginCell()
-        .storeUint(op.transfer, 32)
-        .storeUint(queryId, 64)
-        .storeCoins(jettonAmount)
-        .storeAddress(to)
-        .storeAddress(responseAddress)
-        .storeMaybeRef(customPayload)
-        .storeCoins(forwardTonAmount)
-        .storeMaybeRef(forwardPayload)
-        .endCell();
-}
+import { beginCell, Address, TonClient } from "@ton/ton";
 
 export async function getJettonWalletAddress(
     client: TonClient,
@@ -35,16 +12,4 @@ export async function getJettonWalletAddress(
     );
 
     return result.stack.readAddress();
-}
-
-
-export async function getJettonWalletData(client: TonClient, jettonWalletAddress: Address) {
-    const result = await client.runMethod(jettonWalletAddress, 'get_wallet_data', []);
-
-    return {
-        balance: result.stack.readBigNumber(),
-        owner: result.stack.readAddress(),
-        minter: result.stack.readAddress(),
-        wallet_code: result.stack.readCell()
-    }
 }
