7eacc540e70e6ac08dfa9f2128bb602e7cd4e983
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-14 23:43:46 +0500

    feat: add main sc & jetton wallet op codes

--- src/op.ts
+++ src/op.ts
@@ -1,1 +1,13 @@
-export const transfer = 0xf8a7ea5;
+export const mainOpCodes = {
+    upgrade: 999,
+    updateRootPrice: 344,
+}
+
+export const jettonOpCodes = {
+    transfer: 0xf8a7ea5,
+    transfernotification: 0x7362d09c,
+    internalTransfer: 0x178d4519,
+    excesses: 0xd53276db,
+    burn: 0x595f07bc,
+    burnNotification: 0x7bdd97de,
+}
