ea208af4be785a12cdf10d2f69977e48f0c52db8
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-14 23:44:27 +0500

    feat: update main sc wrapper

--- src/contracts/Main.ts
+++ src/contracts/Main.ts
@@ -1,12 +1,14 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { mainOpCodes } from '../op';
 
 export type MainConfig = {
     usdtJettonMasterAddress: Address,
     rootMasterAddress: Address,
     userScCode: Cell,
     adminAddress: Address,
-    usdtJettonWalletCode: Cell,
+    jettonWalletGovernedCode: Cell,
     jettonWalletCode: Cell,
+    rootPrice: bigint,
 };
 
 export function mainConfigToCell(config: MainConfig): Cell {
@@ -15,8 +17,9 @@ export function mainConfigToCell(config: MainConfig): Cell {
         .storeAddress(config.rootMasterAddress)
         .storeRef(config.userScCode)
         .storeAddress(config.adminAddress)
-        .storeRef(config.usdtJettonWalletCode)
+        .storeRef(config.jettonWalletGovernedCode)
         .storeRef(config.jettonWalletCode)
+        .storeUint(config.rootPrice, 64)
         .endCell();
 }
 
@@ -41,6 +44,31 @@ export class Main implements Contract {
         });
     }
 
+    async sendUpdateRootPrice(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint, newRootPrice: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(mainOpCodes.updateRootPrice, 32)
+                .storeUint(queryId, 64)
+                .storeUint(newRootPrice, 64)
+                .endCell(),
+        });
+    }
+
+    async sendUpgradeContract(provider: ContractProvider, via: Sender, value: bigint, queryId: bigint, newCode: Cell, newData: Cell) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(mainOpCodes.upgrade, 32)
+                .storeUint(queryId, 64)
+                .storeRef(newData)
+                .storeRef(newCode)
+                .endCell()
+        })
+    }
+
     async getUserScAddress(provider: ContractProvider, ownerAddress: Address): Promise<Address> {
         const result = (await provider.get('get_user_sc_address', [
             {
@@ -54,20 +82,15 @@ export class Main implements Contract {
 
     async getData(provider: ContractProvider) {
         const result = (await provider.get('get_main_data', [])).stack
-        let usdtJettonMasterAddress = result.readAddress();
-        let rootMasterAddress = result.readAddress();
-        let userScCode = result.readCell();
-        let adminAddress = result.readAddress();
-        let usdtJettonWalletCode = result.readCell();
-        let jettonWalletCode = result.readCell();
 
         return {
-            usdtJettonMasterAddress,
-            rootMasterAddress,
-            userScCode,
-            adminAddress,
-            usdtJettonWalletCode,
-            jettonWalletCode,
+            usdtJettonMasterAddress: result.readAddress(),
+            rootMasterAddress: result.readAddress(),
+            userScCode: result.readCell(),
+            adminAddress: result.readAddress(),
+            usdtJettonWalletCode: result.readCell(),
+            jettonWalletCode: result.readCell(),
+            rootPrice: result.readBigNumber(),
         }
     }
 }
