276449cb4c121c4576103a0e948e964e1af02353
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-15 22:57:21 +0500

    feat: update beetroot jm & main sc addresses

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const BEETROOT_JETTON_MASTER_ADDRESS = "kQCTZp7dmrlNNgT-Yj-6oJzr5fHZ5s-GutmDVRy_Si6jOttV";
+export const BEETROOT_JETTON_MASTER_ADDRESS = "kQA3ZfqUbMvK7CchhbjhvGi_GRWuopNSSAFIJS8HMtrOyC8T";
 export const USDT_JETTON_MASTER_ADDRESS = "kQBxPxYRD9IOrnIEQysG7D9enWnxg8AGx-wUdcA55mxaKdTK";
-export const MAIN_SC_ADDRESS = "kQBg6rSLYjmIOpKtvVyoQHeFg3SCLkokQsRetmhuDLXX7cmT";
+export const MAIN_SC_ADDRESS = "kQBDe3PHuPZn6125f1_D5tJqZx2rjybVzACxTH27m0KA0ZPf";
 export const TESTNET_CHAIN_ID = "-3";
