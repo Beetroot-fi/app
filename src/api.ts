45789db1cd09efd79e2226e593dd8dd5f496f26f
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-22 23:06:33 +0500

    feat: create api helper

---
+++ src/api.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosInstance } from "axios";
+
+const api: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const apiService = {
+    login: async (initData: string) => {
+        try {
+            const response = await api.post('/v1/auth/login', {
+                "query_str": initData
+            });
+
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    deposit: async (walletAddress: string) => {
+        try {
+            const response = await api.post('/v1/process/deposit', { "wallet_address": walletAddress });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    withdraw: async (walletAddress: string) => {
+        try {
+            const response = await api.post('/v1/process/withdraw', { "wallet_address": walletAddress });
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+};
