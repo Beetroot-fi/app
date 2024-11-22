cb68a93a9f0d2195aac6a530d61d2fc8c97429b7
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-22 23:07:29 +0500

    feat: implement get_main_data get method

--- src/methods/mainScUtils.ts
+++ src/methods/mainScUtils.ts
@@ -10,3 +10,26 @@ export async function getUserScAddress(client: TonClient, userAddress: Address):
 
     return result.stack.readAddress();
 }
+
+
+export async function getMainData(client: TonClient) {
+    const result = (await client.runMethod(
+        Address.parse(MAIN_SC_ADDRESS),
+        'get_main_data',
+        [],
+    )).stack;
+
+    return {
+        usdtJettonMasterAddress: result.readAddress(),
+        rootMasterAddress: result.readAddress(),
+        userScCode: result.readCell(),
+        adminAddress: result.readAddress(),
+        usdtJettonWalletCode: result.readCell(),
+        jettonWalletCode: result.readCell(),
+        rootPrice: result.readBigNumber(),
+        tradoorMasterAddress: result.readAddress(),
+        stormVaultAddress: result.readAddress(),
+        usdtSlpJettonWallet: result.readAddress(),
+        usdtTlpJettonWallet: result.readAddress(),
+    }
+}
