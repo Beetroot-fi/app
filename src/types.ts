5c41dadf3034554a9bc657b0cd23c3713803a8b4
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-26 11:37:26 +0500

    refactor: remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,3 @@
-import { Cell, Address } from "@ton/core";
-
-export type MainDataType = {
-    usdtJettonMasterAddress: Address;
-    rootMasterAddress: Address;
-    userScCode: Cell;
-    adminAddress: Address;
-    usdtJettonWalletCode: Cell;
-    jettonWalletCode: Cell;
-    rootPrice: bigint;
-    tradoorMasterAddress: Address;
-    stormVaultAddress: Address;
-    usdtSlpJettonWallet: Address;
-    usdtTlpJettonWallet: Address;
-};
-
 export type MetricsResponse = {
     tvl: number;
     apy: number;
