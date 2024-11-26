eda6f10f664d5dd0e484f4150438c03154a16e31
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-26 11:37:18 +0500

    feat: remove unused op codes

--- src/op.ts
+++ src/op.ts
@@ -1,11 +1,6 @@
-export const mainOpCodes = {
-    upgrade: 999,
-    updateRootPrice: 344,
-}
-
 export const jettonOpCodes = {
     transfer: 0xf8a7ea5,
-    transfernotification: 0x7362d09c,
+    transferNotification: 0x7362d09c,
     internalTransfer: 0x178d4519,
     excesses: 0xd53276db,
     burn: 0x595f07bc,
