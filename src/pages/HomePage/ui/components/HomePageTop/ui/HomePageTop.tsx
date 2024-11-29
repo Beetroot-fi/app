e47a079780048623780b333a943934dc7791eef2
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-29 11:32:59 +0500

    feat: return

--- src/pages/HomePage/ui/components/HomePageTop/ui/HomePageTop.tsx
+++ src/pages/HomePage/ui/components/HomePageTop/ui/HomePageTop.tsx
@@ -267,7 +267,7 @@ export const HomePageTop = () => {
       <div className={s.roots}>
         <div className={s.root}>
           <p>ROOT PRICE</p>
-          <p>${Number(metrics?.root_price) / 1e2}</p>
+          <p>${metrics?.root_price}</p>
         </div>
         <div className={s.root}>
           <p>ROOT TVL</p>
