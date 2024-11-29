71509f87cd8d7145fb8466ce96dfb77fd0be5377
welaskez <nazar.nikitenko.03@mail.ru>
2024-11-29 13:54:44 +0500

    feat: fixed to 2 numbers

--- src/pages/HomePage/ui/components/HomePageTop/ui/HomePageTop.tsx
+++ src/pages/HomePage/ui/components/HomePageTop/ui/HomePageTop.tsx
@@ -267,7 +267,7 @@ export const HomePageTop = () => {
       <div className={s.roots}>
         <div className={s.root}>
           <p>ROOT PRICE</p>
-          <p>${Number(metrics?.root_price) / 1e4}</p>
+          <p>${(Number(metrics?.root_price) / 1e4).toFixed(2)}</p>
         </div>
         <div className={s.root}>
           <p>ROOT TVL</p>
